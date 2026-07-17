b166992af32214dd3903b7b002822886c9516b04
llamabug <matildajaprea@gmail.com>
2026-07-17 21:32:59 +1200

    update_final

--- script.js
+++ script.js
@@ -1,178 +1,120 @@
-const canvas = document.getElementById("wheel");
-const ctx = canvas.getContext("2d");
-const spinButton = document.getElementById("spin");
-const namesInput = document.getElementById("items");
-const colours = [
-    "#e7c68d",
-    "#ca956c",
-    "#b5ad69",
-    "#e6e0d7",
-    "#fbeea4",
-    "#d6e08d",
-]
-
-const clickSound = new Audio("click.wav");
-const trumpetSound = new Audio("trumpet.mp3");
-
-
-
-let names = [];
-let startAngle = 0;
-let arc;
-let spinTimeout;
-let spinAngleStart = Math.random() * 10 + 10;
-let spinTime = 0;
-let spinTimeTotal = 0;
-let spinning = false;
-let pointerOffset = 0;
-let pointerDirection = 1;
-let previousSegment = 0;
-
-
-
-document.getElementById("closePopup").addEventListener("click", () => {
-        document.getElementById("winnerPopup").style.display = "none";
-    })
-
-
-document.getElementById("removePopup").addEventListener("click", () => {
-    document.getElementById("winnerPopup").style.display = "none";
-    document.getElementById("winnerText").textContent = "";
-})
-
-
-function drawWheel() {
-    
-    const radius = (canvas.width - 5) / 2;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    names.forEach((name, i) => {
-        const angle = startAngle + i * arc;
-        ctx.beginPath();
-        ctx.arc(radius, radius, radius, angle, angle + arc, false);
-        ctx.lineTo(radius, radius);
-        ctx.fillStyle = colours[i % colours.length];
-        ctx.fill();
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        ctx.save();
-        ctx.translate(radius + Math.cos(angle + arc / 2) * (radius / 2), radius + Math.sin(angle + arc / 2) * (radius / 2));
-        ctx.rotate(angle + arc / 2);
-        ctx.fillStyle = "black";
-        ctx.font = "16px Arial";
-        ctx.fillText(name, -ctx.measureText(name).width / 2, 5);
-        ctx.restore();
-    });
-
-    ctx.beginPath();
-    ctx.arc(radius, radius, 20, 0, 2 * Math.PI);
-    ctx.fillStyle = "black";
-    ctx.fill();
-
-    
-    ctx.beginPath();
-    ctx.moveTo(radius - 15 + pointerOffset, 10);
-    ctx.lineTo(radius + 15 + pointerOffset, 10);
-    ctx.lineTo(radius + pointerOffset, 50);
-    ctx.fillStyle = "black";
-    ctx.fill();
-    ctx.font = "20px serif";
-ctx.fillText(" 🥭", radius - 18, 45);
-
+body {
+    font-family: fantasy;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    background: #e5d0b6;
+    color: #6e3a12;
 }
 
-let currentSpeed = 40;
+.container {
+    display: flex;
+}
 
-function rotateWheel() {
-
-    if (currentSpeed <= 0.2) {
-        stopRotateWheel();
-        return;
-    }
-    
-    startAngle += currentSpeed * Math.PI / 180;
-    currentSpeed *= 0.985;
-
-    const currentSegment =
-    Math.floor(((startAngle * 180 / Math.PI) % 360) / (360 / names.length));
-
-    if (currentSegment !== previousSegment) {
-        clickSound.currentTime = 0;
-        clickSound.play();
-        previousSegment = currentSegment;
-    }
-
-    drawWheel();
-    requestAnimationFrame(rotateWheel);
-
+.rbox, .lbox {
+    width: 50vw;
+    text-align: center;
 }
 
 
 
-function stopRotateWheel() {
+h1 {
+    font-size: 45px;
+    text-decoration: underline wavy;
+    text-underline-offset: 6px;
+}
 
-    spinning = false;
+h2 {
+    font-size: 35px;
+}
 
-    const degrees = (startAngle * 180 / Math.PI) % 360;
-    const adjustedDegrees = (360 - degrees) % 360;
-    const index = Math.floor(adjustedDegrees / (360 / names.length));
-    
-    
-    document.getElementById("winnerText").textContent = names[index - 1];
-    document.getElementById("winnerPopup").style.display = "flex";
-    
+p {
+    font-size: 20px;
+}
 
-    trumpetSound.play();
+input {
+    font-size: 20px;
+    padding: 10px;
+    margin: 12px 0;
+    width: 300px;
+    max-width: 80%;
+    border: none;
+    border-radius: 10px;
+    background-color: white;
+    color: #6e3a12;
+    outline-color: #6e3a12;
+    font-family: fantasy;
+}
 
-    confetti({
-        particleCount: 100,
-        spread:70
-    });
+button {
+    font-size: 20px;
+    padding: 10px;
+    margin: 10px 0;
+    width: 300px;
+    max-width: 80%;
+    border: none;
+    border-radius: 10px;
+    background-color: white;
+    color: #6e3a12;
+    cursor: pointer;
+    transition: background-color 0.3s;
+    font-family: fantasy;
+}
 
+button:hover {
+    background-color: #fffcca;
+}
 
+canvas {
+    margin-top: 20px;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    justify-content: center;
+}
+
+canvas:hover {
+    transform: scale(1.05);
 }
 
 
 
-function easeOut(t, b, c, d) {
+.popup {
+    position: fixed;
+    inset: 0;
+    display: none;
+    justify-content: center;
+    align-items: center;
+    z-index: 100;
 
-    const ts = (t /= d) * t;
-    const tc = ts * t;
-    return b + c + (tc + -3 * ts + 3 * t);
-
+    background: rgba(0, 0, 0, 0.5);
 }
 
+.popup-content {
+    background: linear-gradient(
+        135deg,
+        #fbeea4,
+        #e7c68d,
+        #e6e0d7
+    );
+    border: 4px solid #6e3a12;
+    padding: 30px;
+    width: 250px;
+    text-align: center;
+    color: #6e3a12;
+    font-size: 30px;
+}
 
+.popup-content button {
+    background:#e6e0d7;
+    border: solid #6e3a12;
+    padding: 10px 20px;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 16px;
+}
 
-spinButton.addEventListener("click", () => {
-    if (spinning) return; 
-    names = namesInput.value.split(",").map(name => name.trim()).filter(name => name);
-    if (names.length < 2) {
-        alert("Please enter at least 2 items!");
-        return;
-    }
-    currentSpeed = Math.random() * 20 + 30;
-    arc = (2 * Math.PI) / names.length;
-    spinAngleStart = Math.random() * 40 + 40;
-    spinTime = 0;
-    spinTimeTotal = Math.random() * 1000 + 1300;
-    spinning = true;
-    drawWheel();
-    startAngle += Math.random() * Math.PI * 2;
-    rotateWheel();
-
-});
-
-
-namesInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        spinButton.click();
-    }
-});
-
-
-names = ["Code", "Build", "Dream", "Create", "Ship", "Macondo",];
-arc = (2 * Math.PI) / names.length;
-drawWheel();
-
+.popup-content button:hover {
+    background: #fffcca;
+}
